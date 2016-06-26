71ed05134774d332d21f06cdaf62a8a21f231abd
Paul Tagliamonte <tag@pault.ag>
2016-06-25 20:32:47 -0400

    Hello, Sense!

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+long_description = ""
+
+setup(
+    name="sense",
+    version="0.1",
+    packages=['sense'],
+    author="Paul Tagliamonte",
+    author_email="tag@pault.ag",
+    long_description=long_description,
+    description='n/a',
+    license="Expat",
+    url="https://pault.ag/",
+    platforms=['any']
+)
